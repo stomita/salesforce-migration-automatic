08690f2944b0ae5d24b07d02b33c111e9f366714
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-11-07 07:36:38 +0900

    add test case with mapping policy

--- test/index.test.ts
+++ test/index.test.ts
@@ -123,6 +123,123 @@ ${CONTACT_IDS[0]},Sarah,Connor,${ACCOUNT_IDS[0]}
     expect(idMap.size).toBe(0);
   });
 
+  it('should upload data with mapping policy', async () => {
+    const am = new AutoMigrator(conn);
+    const ret1 = await conn
+      .sobject('Account')
+      .create({ Name: 'Account 01', Website: 'example.com' });
+    const ret2 = await conn
+      .sobject('Account')
+      .create({ Name: 'Account 01', Website: 'example.net' });
+    const aid1 = ret1.success ? ret1.id : null;
+    const aid2 = ret2.success ? ret2.id : null;
+    if (!aid1 || !aid2) {
+      throw new Error('Account creation failed');
+    }
+    const {
+      totalCount,
+      successes,
+      failures,
+      blocked,
+      idMap,
+    } = await am.loadCSVData(
+      [
+        {
+          object: 'Contact',
+          csvData: `
+Id,FirstName,LastName,AccountId
+${CONTACT_IDS[0]},John,Doe,${ACCOUNT_IDS[0]}
+${CONTACT_IDS[1]},Jane,Doe,${ACCOUNT_IDS[0]}
+        `.trim(),
+        },
+        {
+          object: 'Account',
+          csvData: `
+Id,Name,Website
+${ACCOUNT_IDS[0]},Account 01,example.com
+        `.trim(),
+        },
+      ],
+      [
+        {
+          object: 'Account',
+          keyFields: ['Name', 'Website'],
+        },
+      ],
+    );
+    expect(totalCount).toBe(3);
+    expect(successes).toBeDefined();
+    expect(successes.length).toBe(2);
+    expect(failures).toBeDefined();
+    expect(failures.length).toBe(0);
+    expect(blocked).toBeDefined();
+    expect(blocked.length).toBe(0);
+    expect(idMap).toBeDefined();
+    expect(idMap.size).toBe(3);
+    expect(idMap.get(ACCOUNT_IDS[0])).toBe(aid1);
+  });
+
+  it('should upload data with default record mapping', async () => {
+    const am = new AutoMigrator(conn);
+    const { user_id: userId } = await conn.identity();
+    await conn.sobject('Account').create({ Name: 'Existing Account' });
+    const {
+      totalCount,
+      successes,
+      failures,
+      blocked,
+      idMap,
+    } = await am.loadCSVData(
+      [
+        {
+          object: 'Contact',
+          csvData: `
+Id,FirstName,LastName,AccountId,OwnerId
+${CONTACT_IDS[0]},John,Doe,${ACCOUNT_IDS[0]},${USER_IDS[0]}
+${CONTACT_IDS[1]},Jane,Doe,${ACCOUNT_IDS[0]},${USER_IDS[1]}
+        `.trim(),
+        },
+        {
+          object: 'Account',
+          csvData: `
+Id
+${ACCOUNT_IDS[0]}
+        `.trim(),
+        },
+        {
+          object: 'User',
+          csvData: `
+Id
+${USER_IDS[0]}
+${USER_IDS[1]}
+        `.trim(),
+        },
+      ],
+      [
+        {
+          object: 'Account',
+          defaultMapping: {
+            condition: "Name = 'Existing Account'",
+            orderby: 'CreatedDate DESC',
+          },
+        },
+        {
+          object: 'User',
+          defaultMapping: userId,
+        },
+      ],
+    );
+    expect(totalCount).toBe(5);
+    expect(successes).toBeDefined();
+    expect(successes.length).toBe(2);
+    expect(failures).toBeDefined();
+    expect(failures.length).toBe(0);
+    expect(blocked).toBeDefined();
+    expect(blocked.length).toBe(0);
+    expect(idMap).toBeDefined();
+    expect(idMap.size).toBe(5);
+  });
+
   it('should upload data from csv', async () => {
     const accCnt = await conn.sobject('Account').count();
     const oppCnt = await conn.sobject('Opportunity').count();
